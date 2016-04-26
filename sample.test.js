e24f32e0de39b22386ce0af92e3b2f669a3ab4cf
swethab <swethab@thoughtworks.com>
2016-04-26 11:19:47 +0530

    addede test file

---
+++ sample.test.js
@@ -0,0 +1,36 @@
+this.browsers = {
+  firefox: {
+    browserName: "firefox"
+  },
+  chrome: {
+    browserName: "chrome"
+  }
+};
+
+this.devices = {
+  mobile: {
+    deviceName: "mobile",
+    size: "400x700"
+  },
+  tablet: {
+    deviceName: "tablet",
+    size: "600x800"
+  },
+  desktop: {
+    deviceName: "desktop",
+    size: "1024x768"
+  }
+};
+
+forAll(devices, function () {
+  	forAll(browsers, function () {
+	    test("Test on ", function (device, browser) {
+		
+	      	var driver = createDriver("http://newsclick.in/india/double-standards-arrest-owaisi-impunity-ramdev", device.size, browser.browserName);
+			  checkLayout(driver, "home-page.gspec");
+			  driver.quit();
+	    });
+	  });
+});
+
+  
